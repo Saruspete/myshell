e1cce45a2dc66e6ffd1d9222427e4f7104309eff
Adrien Mahieux <adrien.mahieux@gmail.com>
2020-05-22 11:10:36 +0200

    [Fix] Load myshell from wrapper

--- myshell.bash
+++ myshell.bash
@@ -2,3 +2,20 @@
 
 # Source system-bashrc
 [[ -e /etc/bashrc ]] && source /etc/bashrc
+
+# -----------------------------------------------------------------------------
+# MyShell Loader
+typeset mys_load=true
+
+# Only work on interactive mode
+[[ ${-//i/} != "$-" ]] && mys_load=false
+# Need a valid homedir
+[[ -z "$HOME" ]] || ! [[ -d "$HOME" ]] && mys_load=false
+
+
+if $mys_load; then
+	source $HOME/.myshell/myshell
+fi
+# End of MyShell loader
+# -----------------------------------------------------------------------------
+
